e081586ca615859e2c5c6f3882a28d2b31105de1
geros78 <alejo.o.o1000@gmail.com>
2022-09-10 13:58:37 -0500

    Inicializado el servidor de express

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+app.get('/', function(req,res){
+    res.send("Hola mundo")
+})
+
+const port= 3000
+app.listen(port,()=> {
+    console.log(`Levantando Server en el puerto: ${port}`)
+})
