e28647dc44815f025eccb69ef4ca39d023cfb225
Alejandro <alejo.o.o1000@gmail.com>
2022-09-10 14:05:53 -0500

    Cambio de usuario

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ app.get('/', function(req,res){
     res.send("Hola mundo")
 })
 
+
+//Se inicializa el servidor
 const port= 3000
 app.listen(port,()=> {
     console.log(`Levantando Server en el puerto: ${port}`)
